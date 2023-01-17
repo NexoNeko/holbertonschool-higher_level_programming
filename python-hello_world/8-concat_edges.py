a57d191c8bc26be084423f145a4c672c6468ef52
NexoNeko <meresidamente@hotmail.com>
2023-01-17 19:43:42 -0300

    removed conditional variable

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str2 = str
-str = str2[39:67] + str2[107:112] + str2[0:6]
+str = (str[39:67] + str[107:112] + str[0:6])
 print(str)
