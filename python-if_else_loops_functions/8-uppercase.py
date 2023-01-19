7d54dd79c38e7fda87825952167228c5636f1b67
NexoNeko <meresidamente@hotmail.com>
2023-01-19 07:19:13 -0800

    Fixed pep8 again

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for character in str:
         i += 1
         endchar = '\n' if i == (len(str)) else ""
-        if ord(character)>=97 and ord(character)<=122:
+        if ord(character) >= 97 and ord(character) <= 122:
             print("{:c}".format(ord(character)-32), end=endchar)
         else:
             print(f"{character}", end=endchar)
