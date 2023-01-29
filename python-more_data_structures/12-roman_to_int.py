4a4364c81765e9b9dccc88dec90d96042f320976
NexoNeko <meresidamente@hotmail.com>
2023-01-29 10:11:09 -0300

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,10 @@
 
 
 def roman_to_int(roman_string):
+
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+
     c = []
     res = 0
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
