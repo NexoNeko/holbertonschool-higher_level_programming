a0a1f49022e4a952a28d23ed15ca418d2e89ca23
NexoNeko <meresidamente@hotmail.com>
2023-02-02 20:38:43 -0300

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
 """Defines the class square """
+
+
 class Square:
-    """ a square with defined size """
+    """This is a class Square."""
     def __init__(self, size=0):
+        """ Initializes the square """
         self.__size = size
-    
+
     @property
     def size(self):
         return (self.__size)
-    
-    #this is now setting its value
+
+    # this is now setting its value
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -18,13 +20,13 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
-    """ The area method returns the area """
+
     def area(self):
+        """ The area method returns the area """
         return self.__size ** 2
-    
-    """ This prints the square """
+
     def my_print(self):
+        """ This prints the square """
         if self.__size > 0:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
