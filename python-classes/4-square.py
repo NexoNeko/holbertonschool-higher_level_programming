f30baa5372a7851b36b41697cba4486706b2289e
NexoNeko <meresidamente@hotmail.com>
2023-02-02 20:36:00 -0300

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
 """Defines the class square """
+
+
 class Square:
-    """ a square with defined size """
-    #this initializes the square
+    """This is a class Square."""
     def __init__(self, size=0):
+        """ Initializes the square """
         self.__size = size
     
     @property
@@ -19,6 +20,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    """ The area method returns the area """
+
     def area(self):
+        """ The area method returns the area """
         return self.__size ** 2
