59d061789712f3549ab25e517b038dbc338cdc3f
NexoNeko <meresidamente@hotmail.com>
2023-02-02 20:33:09 -0300

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-
 """Defines the class square """
+
+
 class Square:
-    """ a square with defined size """
+    """This is a class Square."""
     def __init__(self, size=0):
+        """ Initializes the square """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    """ The area method returns the area """
+    
     def area(self):
+        """ The area method returns the area """
         return self.__size ** 2
