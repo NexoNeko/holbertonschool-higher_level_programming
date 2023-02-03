d0738bbfa236fb8b4c9c2c86e7bc5158592fed14
NexoNeko <meresidamente@hotmail.com>
2023-02-03 20:03:16 -0300

    moretests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     Casts the values into int before adding"""
 
     if type(a) is not int\
-    and type(a) is not float:
+            and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int\
-    and type(b) is not float:
+            and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
