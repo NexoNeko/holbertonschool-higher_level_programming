36cff055140e7729ec46723196d12063dc5dfc20
NexoNeko <meresidamente@hotmail.com>
2023-02-03 19:58:25 -0300

    exercise 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """ Returns the addition of a + b.
+    Raises a type error if A or B are not ints
+    Casts the values into int before adding"""
+
+    if type(a) is not int\
+    and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int\
+    and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
