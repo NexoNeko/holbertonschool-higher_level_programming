b5feeb5d812a5e012c5d8e51e034897d14475830
NexoNeko <meresidamente@hotmail.com>
2023-02-02 21:08:49 -0300

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """ Initializes the square """
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
