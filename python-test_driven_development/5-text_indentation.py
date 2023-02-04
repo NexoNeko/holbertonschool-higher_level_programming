f2d8b1dc425c647bdabeed2611208d449ee5464c
NexoNeko <meresidamente@hotmail.com>
2023-02-04 17:12:25 -0300

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,24 +2,31 @@
 """Defines fun that prints 2 new lines after ., ? or :"""
 
 def text_indentation(text):
+"""prints 2 new lines after ., ? or :
+    Removes spaces at begining of new line.
+    If text is not string, throws a type error"""
 
     if type(text) is not str:
         raise TypeError("text must be a string")
 
+    spc_flag = 0
     la = []
+
     for i in text:
+        # this removes spaces
+        if spc_flag == 1:
+            if i == " ":
+                continue
+            spc_flag = 0
+
         if i == "."\
                 or i == "?"\
                 or i == ":":
             la.append(i)
             la.append("\n\n")
+            # sets flag for space removal
+            spc_flag = 1
         else:
             la.append(i)
-
-    for i in range(len(la)):
-        iplus = i - 1
-        if la[i] == " "\
-                and la[iplus] == '\n':
-            la.pop(i)
 
     print("".join(la))
