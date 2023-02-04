ce15b2fefc6c0373c4ba1cfdc2ef00e75ebd690d
NexoNeko <meresidamente@hotmail.com>
2023-02-04 18:27:16 -0300

    Update 6-max_integer_test.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,7 +11,7 @@ class test_max_integer(unittest.TestCase):
     
     def test_string(self):
         """ Test for strings """
-            assertEqual(max_integer("buttercup"), u)
+        assertEqual(max_integer("buttercup"), u)
     
     def test_one_element(self):
         """ Tests for an 1 element list"""
@@ -19,29 +19,36 @@ class test_max_integer(unittest.TestCase):
         assertEqual(max_integer(list), 125)
     
     def test_float(self):
+        """ Test for floats """
         list = [125.5, 154.2, 12.5]
         assertEqual(max_integer(list), 154.2)
     
     def test_float_int(self):
+        """ Test for mixed float and int """
         list = [125.5, 154, 12.5, 3]
         assertEqual(max_integer(list), 154)
     
     def test_float_int_infinity(self):
+        """ Test for float, int and infinity """
         list = [125.5, 154, 12.5, float('inf')]
         assertEqual(max_integer(list), inf)
     
     def test_string_list(self):
+        """ Test for a list of strings """
         list = ["clouds", "flowers", "daggers"]
         assertEqual(max_integer(list), "flowers")
     
     def test_number_string_first_is_greatest(self):
+        """ Test for a list of strings containing numbers """
         list = ['15', '14', '12', '14']
         assertEqual(max_integer(list), 15)
     
     def test_unordered_list(self):
+        """ Test for an unordered list """
         list = [11, 14, 12]
         assertEqual(max_integer(list), 14)
     
     def test_ordered_list(self):
+        """ Test for an ordered list """
         list = [11, 12, 13, 14]
         assertEqual(max_integer(list), 14)
