e5425f6943a4dfcdd35e762cc7767e68aa23a9ab
NexoNeko <meresidamente@hotmail.com>
2023-02-04 15:12:18 -0300

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,7 @@ def say_my_name(first_name, last_name=""):
     if type(first_name) is not string:
         raise TypeError("first_name must be a string")
     if type(last_name) is not string:
-    raise TypeError("last_name must be a string")
+        raise TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}")
 
