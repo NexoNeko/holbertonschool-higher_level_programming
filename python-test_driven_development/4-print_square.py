0ef42fda4a47f74c4ea8035c576b5d813a99d089
NexoNeko <meresidamente@hotmail.com>
2023-02-04 16:16:09 -0300

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,13 +5,10 @@
 def print_square(size):
     """Prints a square of <size> area using char #"""
 
-    if type(size) is not int\
-            and type(size) is not float:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
 
     if size < 0:
-        if type(size) is float:
-            raise TypeError("size must be an integer")
         raise ValueError("size must be >= 0")
 
     for i in range(size):
