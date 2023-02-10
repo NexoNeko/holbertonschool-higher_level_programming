92d02cb13bd1b6e7d8d2b3c6a13903bf390e6ac7
NexoNeko <meresidamente@hotmail.com>
2023-02-10 07:15:56 -0800

    6 done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines the Base Geometry class """
+
+
+class BaseGeometry:
+    """ Raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
