5f7053564499ba734c7bc4ad6d052430eb07cd6f
NexoNeko <meresidamente@hotmail.com>
2023-02-10 06:20:29 -0800

    4 done

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines the inherits from class"""
+
+
+def inherits_from(obj, a_class):
+    """ Returns true if  the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False."""
+    if isinstance(obj, a_class):
+        if issubclass(a_class, type(obj)):
+             return False
+        return True
