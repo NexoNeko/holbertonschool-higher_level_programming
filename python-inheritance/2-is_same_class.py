cd5261b3f66f6cfefa4d21c6f9dce6c6a52d850c
NexoNeko <meresidamente@hotmail.com>
2023-02-10 06:00:29 -0800

    2 done

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines the is_same_class abstract class """
+
+
+def is_same_class(obj, a_class):
+    """ checks if a given object is exactly the given class"""
+    if type(obj) == a_class:
+        return True
