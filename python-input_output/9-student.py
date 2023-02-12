57f3b0ceccdd6dd51da305220090bc558b76bf7f
NexoNeko <meresidamente@hotmail.com>
2023-02-12 04:31:04 -0800

    9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines the class student """
+
+class Student:
+    """ represents a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes the student object """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """ Returns a dict repr of the student obj """
+            return self.__dict__
