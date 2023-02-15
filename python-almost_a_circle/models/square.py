fcb130587a6a461f8f3eba64556b2250e3daeb4f
NexoNeko <meresidamente@hotmail.com>
2023-02-15 08:51:32 -0800

    Updated size set and get

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,5 +5,20 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """The square class, represents a square"""
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+    def __init__(self, Size, x=0, y=0, id=None):
+        super().__init__(Size, Size, x, y, id)
+        self.Size = Size
+
+    @property
+    def size(self):
+        return self.Size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+        self.Size = value
