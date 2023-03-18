a8c1f617cee112d02e86b80121832951c3896353
NexoNeko <meresidamente@hotmail.com>
2023-03-18 18:51:23 -0300

    Update 15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,5 @@
 --  lists all Comedy shows in the database hbtn_0d_tvshow
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_show_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
