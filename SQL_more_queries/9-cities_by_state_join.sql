ab6aceed6b2f63c4bf82e9ae6351c4e6cbe59cb3
NexoNeko <meresidamente@hotmail.com>
2023-03-18 16:12:49 -0300

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa according to instructions.
-SELECT id, name, states.name FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+ SELECT cities.id, cities.name, states.name FROM cities
+ JOIN states ON cities.state_id = states.id
+ ORDER BY cities.id ASC;
