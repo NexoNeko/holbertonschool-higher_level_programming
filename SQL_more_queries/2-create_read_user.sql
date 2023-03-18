28a9ed895f80e21ec726cfe48d0d2d13e99b8971
NexoNeko <meresidamente@hotmail.com>
2023-03-18 11:42:25 -0300

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
-/* creates the database hbtn_0d_2 and the user user_0d_2, with
-nothing but SELECT privilege.
-Password set to user_0d_2_pwd
-if database or user exists, script doesn't fails.*/
+-- creates the database hbtn_0d_2 and the user user_0d_2, with
+-- nothing but SELECT privilege.
+-- Password set to user_0d_2_pwd
+-- if database or user exists, script doesn't fails.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
