153b7d57073da71b3696e5959c12b540c653bb14
NexoNeko <meresidamente@hotmail.com>
2023-03-23 19:16:25 -0300

    Update model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# Defines a City model.
+""" City Module for HBTN project """
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class City(Base):
     """ Represents a city for a MySQL database. """
