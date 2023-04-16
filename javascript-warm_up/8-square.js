38370f7a4bcce7b2de3b1b35d9550890f9bc31ab
NexoNeko <meresidamente@hotmail.com>
2023-04-16 14:50:10 -0300

    semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-let size = Math.floor(Number(process.argv[2]));
+const size = Math.floor(Number(process.argv[2]));
 
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
-      for (let c = 0; c < size - 1; c++) {
-        process.stdout.write('X');
-      }
-      console.log('X');
+    for (let c = 0; c < size - 1; c++) {
+      process.stdout.write('X');
+    }
+    console.log('X');
   }
 }
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,5 +4,5 @@ function add (a, b) {
   return a + b;
 }
 
-result = add(Number(process.argv[2]), Number(process.argv[3]));
+const result = add(Number(process.argv[2]), Number(process.argv[3]));
 console.log(result);
