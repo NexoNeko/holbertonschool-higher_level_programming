9d273274e55b88e919d309e8506c54a528f99206
NexoNeko <meresidamente@hotmail.com>
2023-04-16 14:35:10 -0300

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let argumentCount = process.argv.length;
+const argumentCount = process.argv.length;
 
 if (argumentCount < 3){
-	console.log("No argument");
-}else if (argumentCount === 3){
-	console.log("Argument found");
-}else{
-	console.log("Arguments found");
+  console.log('No argument');
+} else if (argumentCount === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
