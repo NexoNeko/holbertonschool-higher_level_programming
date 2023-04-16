090af57a614f93338df0c5e3e25e54d14a5ae340
NexoNeko <meresidamente@hotmail.com>
2023-04-16 14:31:36 -0300

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
-let args = process.argv.map(Number)
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
     .slice(2, process.argv.length)
-
-console.log(args.sort((a, b) => a - b).slice(-2, -1));
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
