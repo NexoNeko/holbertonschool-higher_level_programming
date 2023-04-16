f840e1f2be38252529ce5635246906e555513cd9
NexoNeko <meresidamente@hotmail.com>
2023-04-16 14:39:50 -0300

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-let inputValue = Math.floor(Number(process.argv[2]));
+const inputValue = Math.floor(Number(process.argv[2]));
 console.log(isNaN(inputValue) ? 'Not a number' : `My number: ${inputValue}`);
