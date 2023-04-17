f9d339594568568f924911bfcd800817ed123d2b
NexoNeko <meresidamente@hotmail.com>
2023-04-17 12:11:55 -0300

    Update 2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log();
+  }
   console.log('code:', response.statusCode); // Print the response status code if a response was received
 });
