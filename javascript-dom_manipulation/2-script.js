0677c7f4c9a0676170b45b9041023228f282ebc0
NexoNeko <meresidamente@hotmail.com>
2023-04-18 18:54:12 -0300

    3

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,5 +2,5 @@ const header = document.querySelector('header');
 const redHeader = document.getElementById('red_header');
 
 redHeader.addEventListener('click', function () {
-  header.style.color = '#FF0000';
+  header.classList.add('red');
 });
