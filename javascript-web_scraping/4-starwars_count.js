84e62a49d3f2efa0e386551a98fbe4207fa76e0e
NexoNeko <meresidamente@hotmail.com>
2023-04-17 21:43:42 -0300

    correct logic

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,6 +7,6 @@ request(film, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   }
-  const x = JSON.stringify(body);
-  console.log(x.match(/people\/18\//g).length);
+  const x = JSON.stringify(body).match(/people\/18\//g);
+  console.log(x === null ? 0 : x.length);
 });
