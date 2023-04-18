2faf34f07990a76b090acee1a276a6609dada51b
NexoNeko <meresidamente@hotmail.com>
2023-04-18 19:23:08 -0300

    Update 6-script.js

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,16 +1,5 @@
-const character = document.querySelector('character');
-const updateHeader = document.querySelector('#update_header');
-const request = require('request');
-
-
-updateHeader.addEventListener('click', () => {
-	const film = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
-	request(film, function (error, response, body) {
-	  if (error) {
-		console.log('error:', error);
-	  }
-	  console.log(response);
-	  character.innerText = JSON.stringify(body.name);
-	});
-});
-
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('character').innerHTML = data.name;
+  });
